7f7ee07e189d6185c0d9d1db2899ceb94ba4c4a1
Oz4 <aowss18@gmail.com>
2022-08-12 22:32:26 +0300

    added: native navigation bar color

--- src/setup/Providers.tsx
+++ src/setup/Providers.tsx
@@ -1,9 +1,11 @@
 
 import React, { useRef } from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { extendTheme, NativeBaseProvider } from "native-base";
+import { extendTheme, NativeBaseProvider, StatusBar, } from "native-base";
 //@ts-ignore
 import { TailwindProvider } from 'tailwindcss-react-native';
+import * as NavigationBar from 'expo-navigation-bar';
+
 
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
@@ -21,12 +23,15 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         }
     });
 
+    NavigationBar.setBackgroundColorAsync("#121619");
+    NavigationBar.setVisibilityAsync('hidden');
 
     return (
 
         <QueryClientProvider client={reactQueryClient.current}>
             <NativeBaseProvider theme={nativeBaseTheme}>
                 <TailwindProvider>
+                    <StatusBar barStyle={"light-content"} backgroundColor={"#121619"} />
                     {children}
                 </TailwindProvider>
             </NativeBaseProvider>
