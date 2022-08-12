6722106907da9dc218f67c104b5ae4e26ed03838
Oz4 <aowss18@gmail.com>
2022-08-12 13:32:49 +0300

    refactor: list item refactoring

--- src/components/ranking/ListItem.tsx
+++ src/components/ranking/ListItem.tsx
@@ -36,94 +36,117 @@ const ListItem = ({
 }: Props) => {
 
     const [show, setShow] = useState(false)
+
+    const CollectionIndex = () => (
+        <Text className="-text--lr-colors-text-01 mr-2 w-8 flex-shrink-0 text-center" >{index}</Text>
+    )
+
+    const CollectionImage = () => (
+        src ? <Image source={{ uri: src }} className="w-10 h-10" />
+            : <View className="w-10 h-10" />
+    )
+
+    const CollectionName = () => (
+        <View className='flex-row items-center justify-center'>
+            <Text className="-text--lr-colors-text-01 mr-1">
+                {name}
+            </Text>
+            {verified && <MaterialIcons name="verified" size={16} color="#4589FF" />}
+        </View>
+    )
+
+    const CollectionDailyVolume = () => (
+        <View className="flex-col items-center justify-center">
+            <Text className="-text--lr-colors-text-03 text-xs">
+                {convertWeiToEther(dailyVol || "0")}
+            </Text>
+
+            {dailyVolChange === 0 && <Text className="-text--lr-colors-text-03 text-xs">{dailyVolChange.toFixed(2)}%</Text>}
+            {(dailyVolChange && dailyVolChange > 0) ? <Text className="-text--lr-colors-link-01 text-xs">+{dailyVolChange.toFixed(2)}%</Text> : null}
+            {(dailyVolChange && dailyVolChange < 0) ? <Text className="-text--lr-colors-text-error text-xs">{dailyVolChange.toFixed(2)}%</Text> : null}
+        </View>
+    )
+
+    const ShowExtraDetailsButton = () => (
+        <LRButton title={show ? "- Less" : "+ More"} onPress={() => setShow(prev => !prev)} backgroundColor="#00000000" borderColor='#00000000' width={100} height={25} padding={0} color="#878D96" fontSize={12} ml={-30} />
+    )
+
+    const TotalVolume = () => (
+        <View className='flex-row w-36'>
+            <Text className="-text--lr-colors-text-03 text-xs w-20" >
+                Total Vol
+            </Text>
+            <Text className="-text--lr-colors-text-03 text-xs" >
+                {convertWeiToEther(totalVol || "0")}
+            </Text>
+        </View>
+    )
+
+    const Floor = () => (
+        <View className='flex-row'>
+            <Text className="-text--lr-colors-text-03 text-xs w-20" >
+                Floor
+            </Text>
+            <Text className="-text--lr-colors-text-03 text-xs mr-2" >
+                {convertWeiToEther(floor || "0")}
+            </Text>
+
+            {floorChange === 0 && <Text className="-text--lr-colors-text-03 text-xxs mt-px">({floorChange}%)</Text>}
+            {(floorChange && floorChange > 0) ? <Text className="-text--lr-colors-link-01 text-xxs mt-px">(+{floorChange.toFixed(2)}%)</Text> : null}
+            {(floorChange && floorChange < 0) ? <Text className="-text--lr-colors-text-error text-xxs mt-px">({floorChange.toFixed(2)}%)</Text> : null}
+        </View>
+    )
+
+    const Owners = () => (
+        <View className='flex-row w-36'>
+            <Text className="-text--lr-colors-text-03 text-xs w-20" >
+                Owners
+            </Text>
+            <Text className="-text--lr-colors-text-03 text-xs" >
+                {owners}
+            </Text>
+        </View>
+    )
+
+    const Items = () => (
+        <View className='flex-row w-36'>
+            <Text className="-text--lr-colors-text-03 text-xs w-20" >
+                Items
+            </Text>
+            <Text className="-text--lr-colors-text-03 text-xs" >
+                {items}
+            </Text>
+        </View>
+    )
+
+    const HiddenExtraDetails = () => (
+        show ?
+            <View className="flex-col ml-20 pl-3">
+                <Floor />
+                <TotalVolume />
+                <Owners />
+                <Items />
+            </View>
+            :
+            <></>
+    )
+
     return (
         <View className="flex-col py-8 border-b -border-b--lr-colors-border-01">
 
             <View className="flex-row items-center justify-between">
-
                 <View className="flex-row items-center">
-                    <Text className="-text--lr-colors-text-01 mr-2 w-8 flex-shrink-0 text-center" >{index}</Text>
-
-                    {src ? <Image source={{
-                        uri: src,
-                    }}
-                        className="w-10 h-10"
-                    />
-                        : <View className="w-10 h-10" />
-                    }
-
+                    <CollectionIndex />
+                    <CollectionImage />
                     <View className="flex-col ml-2 justify-start">
-                        <View className='flex-row items-center justify-center'>
-                            <Text className="-text--lr-colors-text-01 mr-1">
-                                {name}
-                            </Text>
-                            {verified && <MaterialIcons name="verified" size={16} color="#4589FF"/>}
-                        </View>
-                        <LRButton title={show ? "- Less" : "+ More"} onPress={() => setShow(prev => !prev)} backgroundColor="#00000000" borderColor='#00000000' width={100} height={25} padding={0} color="#878D96" fontSize={12} ml={-30} />
+                        <CollectionName />
+                        <ShowExtraDetailsButton />
                     </View>
                 </View>
-
-                <View className="flex-col items-center justify-center">
-                    <Text className="-text--lr-colors-text-03 text-xs">
-                        {convertWeiToEther(dailyVol || "0")}
-                    </Text>
-
-                    {dailyVolChange === 0 && <Text className="-text--lr-colors-text-03 text-xs">{dailyVolChange.toFixed(2)}%</Text>}
-                    {(dailyVolChange && dailyVolChange > 0) ? <Text className="-text--lr-colors-link-01 text-xs">+{dailyVolChange.toFixed(2)}%</Text> : null}
-                    {(dailyVolChange && dailyVolChange < 0) ? <Text className="-text--lr-colors-text-error text-xs">{dailyVolChange.toFixed(2)}%</Text> : null}
-
-                </View>
-
+                <CollectionDailyVolume />
             </View>
 
-
-            {
-                show &&
-                <View className="flex-col ml-20 pl-3">
-                    <View className='flex-row'>
-                        <Text className="-text--lr-colors-text-03 text-xs w-20" >
-                            Floor
-                        </Text>
-                        <Text className="-text--lr-colors-text-03 text-xs mr-2" >
-                            {convertWeiToEther(floor || "0")}
-                        </Text>
-
-                        {floorChange === 0 && <Text className="-text--lr-colors-text-03 text-xxs mt-px">({floorChange}%)</Text>}
-                        {(floorChange && floorChange > 0) ? <Text className="-text--lr-colors-link-01 text-xxs mt-px">(+{floorChange.toFixed(2)}%)</Text> : null}
-                        {(floorChange && floorChange < 0) ? <Text className="-text--lr-colors-text-error text-xxs mt-px">({floorChange.toFixed(2)}%)</Text> : null}
-                    </View>
-
-                    <View className='flex-row w-36'>
-                        <Text className="-text--lr-colors-text-03 text-xs w-20" >
-                            Total Vol
-                        </Text>
-                        <Text className="-text--lr-colors-text-03 text-xs" >
-                            {convertWeiToEther(totalVol || "0")}
-                        </Text>
-
-
-                    </View>
-
-                    <View className='flex-row w-36'>
-                        <Text className="-text--lr-colors-text-03 text-xs w-20" >
-                            Owners
-                        </Text>
-                        <Text className="-text--lr-colors-text-03 text-xs" >
-                            {owners}
-                        </Text>
-                    </View>
-
-                    <View className='flex-row w-36'>
-                        <Text className="-text--lr-colors-text-03 text-xs w-20" >
-                            Items
-                        </Text>
-                        <Text className="-text--lr-colors-text-03 text-xs" >
-                            {items}
-                        </Text>
-                    </View>
-
-                </View>
-            }
+            <HiddenExtraDetails />
 
         </View >
     )
