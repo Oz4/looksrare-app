542354d3d3f491e0cc5da254b82385c6aa946cdc
Oz4 <aowss18@gmail.com>
2022-08-19 14:14:49 +0300

    updated: show more - less

--- src/components/collection/Description.tsx
+++ src/components/collection/Description.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react"
+import { TouchableOpacity, View } from "react-native"
 import { LRText } from "src/components/common"
+import { shortenString } from "src/utils"
+
+const MAX_LEN = 30
 
 const Description = ({
     description
-}:{
+}: {
     description: string
 }) => {
 
+    const [showMore, setShowMore] = useState(false)
+
+    const ShowMoreButton = () => (
+        <TouchableOpacity onPress={_ => setShowMore(prev => !prev)}>
+            <LRText className="-text--lr-colors-interactive-01" weight="SemiBold">
+                {!showMore ? "More" : "Less"}
+            </LRText>
+        </TouchableOpacity>
+    )
+
+    if (description.length <= 0) return <></>
+
+    const className = showMore ? "mb-4 w-full flex-col" : "mb-4 w-full flex-row flex-wrap"
+    
     return (
-        <LRText className='-text--lr-colors-text-02 text-sm'>
-            {description}
-        </LRText>
+        <View className={className}>
+            <LRText className='-text--lr-colors-text-02 text-sm'>
+                {showMore ? description : shortenString(description, MAX_LEN)}
+            </LRText>
+            {description.length > MAX_LEN && <ShowMoreButton />}
+        </View>
     )
 }
 
