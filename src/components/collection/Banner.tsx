ac4fc0efd6acdbe7900f16a08c876ba148e8d8be
Oz4 <aowss18@gmail.com>
2022-08-19 14:14:22 +0300

    updated: onLoad + placeholder

--- src/components/collection/Banner.tsx
+++ src/components/collection/Banner.tsx
@@ -1,19 +1,53 @@
+import { Skeleton } from "native-base";
+import { useState } from "react";
 import { Image } from "react-native";
 
 const Banner = ({
     uri
 }: {
-    uri: string
+    uri: string | null
 }) => {
-    return (
 
-        <Image source={
-            {
-                uri
-            }
-        }
-            className="w-full h-[96px] object-cover -mb-10"
-        ></Image>
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    if (!isLoaded)
+        return (
+            <>
+                <Skeleton h={"96px"} w={"100%"} startColor="#121619" endColor={"#343A3F"} className="-mb-10" />
+                {
+                    uri &&
+                    <Image
+                        source={
+                            {
+                                uri
+                            }
+                        }
+                        className="w-full h-[96px] object-cover -mb-10 hidden"
+                        onLoad={() => { setIsLoaded(true) }}
+                    />
+                }
+            </>
+        )
+
+
+    return (
+        uri ?
+            <Image
+                source={
+                    {
+                        uri
+                    }
+                }
+                className="w-full h-[96px] object-cover -mb-10"
+                onLayout={() => { setIsLoaded(true) }}
+            />
+            :
+            <Image
+                source={
+                    require("src/assets/looksrarePlaceHolderDark.jpg")
+                }
+                className="w-full h-[96px] object-cover -mb-10"
+            />
     )
 }
 
--- src/components/collection/CollectionImage.tsx
+++ src/components/collection/CollectionImage.tsx
@@ -1,21 +1,57 @@
+import { Skeleton } from "native-base";
+import { useState } from "react";
 import { Image } from "react-native";
 
 
-const CollectionImage = ({ uri }: { uri: string }) => {
+const CollectionImage = ({ uri }: { uri: string | null }) => {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    if (!isLoaded)
+        return (
+            <>
+                <Skeleton h={"92px"} w={"92px"} startColor="#121619" endColor={"#343A3F"} className="ml-4 border-[2px]" />
+                {
+                    uri &&
+                    <Image
+                        source={
+                            {
+                                uri
+                            }
+                        }
+                        className="w-[92px] h-[92px] object-cover ml-4 border-[2px] hidden"
+                        onLoad={() => { setIsLoaded(true) }}
+                    />
+                }
+            </>
+        )
 
     return (
-        <Image source={
-            {
-                uri
+        uri ?
+            <Image source={
+                {
+                    uri
+                }
             }
-        }
-            className="w-[92px] h-[92px] object-cover ml-4 border-[2px]"
-            style={{
-                borderRadius: 4,
-                borderColor: '#121619',
-                borderWidth: 2
-            }}
-        />
+                className="w-[92px] h-[92px] object-cover ml-4 border-[2px]"
+                style={{
+                    borderRadius: 4,
+                    borderColor: '#121619',
+                    borderWidth: 2
+                }}
+            />
+            :
+            <Image
+                source={
+                    require("src/assets/looksrarePlaceHolderDark.jpg")
+                }
+                className="w-[92px] h-[92px] object-cover ml-4 border-[2px]"
+                style={{
+                    borderRadius: 4,
+                    borderColor: '#121619',
+                    borderWidth: 2
+                }}
+            />
     )
 }
 
